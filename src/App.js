5e51491b079d3eed3dedab2fdcc099bed9d7b7a2
hillngeorge <hillngeorge@gmail.com>
2024-03-16 17:06:53 -0500

    108 #2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-
-import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Catalog from './pages/Catalog'; 
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Catalog from "./pages/Catalog";
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-            
+      <Navbar />
 
-            <Catalog/>
+      <Catalog />
 
-
-
-            <Footer/>
+      <Footer />
     </div>
   );
 }
